a30d79997e51bdc221aa53479505b1651b522d56
Mykle99 <jemyklebusten@hotmail.com>
2023-08-28 13:38:03 +0200

    changed imports to adhere to new interface structure

--- tests/test_intention_model.py
+++ tests/test_intention_model.py
@@ -4,20 +4,24 @@ need to be built. See docs on the submodules
 """
 
 from colav_simulator.simulator import Simulator
+from colav_simulator.core.colav.im import IMInterface as imI 
+
 import colav_simulator.core.colav.colav_interface as ci
-import colav_simulator.core.colav.cpp_to_py_interfaces.intention_model_interface.build.parameters as im_param
 import colav_simulator.core.guidances as guidance
+
+
 
 if __name__ == "__main__":
 
     colav_config = ci.Config()
     colav_config.layer1 = ci.LayerConfig()
-    colav_config.layer1.im = im_param.IntentionModelParameters()
+    colav_config.layer1.im = imI.IMParams.IntentionModelParameters()
     colav_config.layer2 = ci.LayerConfig()
     colav_config.layer2.los = guidance.LOSGuidanceParams()
     colav_config.name = ci.COLAVType.IM
     colav_builder = ci.COLAVBuilder()
     simulator = Simulator()
 
+    # use plot_static MATLAB file, a file should be generated to the output folder after running the simulator
     output = simulator.run(ownship_colav_system = colav_builder.construct_colav(config= colav_config))
     print("done")

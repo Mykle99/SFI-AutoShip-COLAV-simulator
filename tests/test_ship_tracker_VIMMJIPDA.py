f0ac4d95addfa8ad0bc62e620f89d9590fce066b
Ragnar Norbye Wien <ragnarnw@stud.ntnu.no>
2024-01-30 17:56:15 +0100

    storing before retrieving newest version of main into branch

--- tests/test_ship_tracker_VIMMJIPDA.py
+++ tests/test_ship_tracker_VIMMJIPDA.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     scenario_generator = ScenarioGenerator()
     scenario_data = scenario_generator.generate(config_file=scenario_file)
     simulator = Simulator()
-    simulator.toggle_liveplot_visibility(False)
+    simulator.toggle_liveplot_visibility(True)
     output = simulator.run([scenario_data])
     print("done")
 
@@ -72,19 +72,19 @@ if __name__ == "__main__":
     
     
 
-    folder_path = '/home/ragnar/Github_test/Plotting_SR_2023/'
-    folder_path_backup = '/home/ragnar/Github_test/Plotting_SR_2023/Backup/'
-    # file_name = 'Test3_VIMMJIPDA'
-    # file_name_backup = 'Test3_VIMMJIPDA'
-    file_name = 'Test3_KF'
-    file_name_backup = 'Test3_KF'
+    # folder_path = '/home/ragnar/Github_test/Plotting_SR_2023/'
+    # folder_path_backup = '/home/ragnar/Github_test/Plotting_SR_2023/Backup/'
+    # # file_name = 'Test3_VIMMJIPDA'
+    # # file_name_backup = 'Test3_VIMMJIPDA'
+    # file_name = 'Test3_KF'
+    # file_name_backup = 'Test3_KF'
 
-    # 1000 funker ikke av en eller annen grunn, så holder meg til 500 enn så lenge
-    with open(folder_path + file_name, 'wb') as file:
-        pickle.dump(output[0]['episode_simdata_list'], file)
+    # # 1000 funker ikke av en eller annen grunn, så holder meg til 500 enn så lenge
+    # with open(folder_path + file_name, 'wb') as file:
+        # pickle.dump(output[0]['episode_simdata_list'], file)
 
-    with open(folder_path_backup + file_name_backup, 'wb') as file:
-        pickle.dump(output[0]['episode_simdata_list'], file)
+    # with open(folder_path_backup + file_name_backup, 'wb') as file:
+        # pickle.dump(output[0]['episode_simdata_list'], file)
 
     # folder_path = '/home/ragnarnw/Github_test/Plotting_SR_2023/'
     # file_name_backup = 'Test1_termination_VIMMJIPDA'
